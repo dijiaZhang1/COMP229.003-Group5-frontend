ed9b5ca87bc831255fb31c1416f3367f8f2fe02c
dijiaZhang1 <97812327+dijiaZhang1@users.noreply.github.com>
2022-04-05 00:08:35 -0400

    update

--- src/app/components/survey/list.component.ts
+++ src/app/components/survey/list.component.ts
@@ -26,3 +26,51 @@ export class ListComponent {
         }
     }
 }
+
+
+/*
+import { Component,OnInit  } from "@angular/core";
+import { Router } from "@angular/router";
+import { Survey } from "../../models/survey.model";
+import { SurveyRepository } from "../../models/survey.repository";
+import {RestDataSource} from '../../models/rest.datasource';
+
+@Component({
+    selector: "list-survey",
+    templateUrl: "list.component.html"
+})
+
+export class ListComponent implements OnInit {
+
+    title = 'Survey List';
+    dataSource:any = [];
+
+    // constructor(private repository: SurveyRepository,
+    //     private router: Router) 
+    // { }
+
+constructor(private restDataSource:RestDataSource) {
+
+}
+
+    ngOnInit():void {
+        this.restDataSource.getSurveyList().subscribe((data:any) => {
+            console.log(data);
+            this.dataSource = data;
+        });
+    }
+
+    
+
+    // get surveyList(): Survey[] {
+    //     return this.repository.getSurvey();        
+    // }
+    
+    // deleteMethod(id: string) {
+    //     if(confirm("Are you sure do you want to delete?")) {
+    //         this.router.navigateByUrl("survey/delete/"+id);
+    //     }
+    // }
+}
+
+*/
---
+++ src/app/models/surveyAnswer.model.ts
@@ -0,0 +1,11 @@
+export class SurveyAnswer {
+    
+    constructor(
+        public _id?: string,
+        public surveyId?: string,
+        public answer?: string[]
+    ){
+    }
+
+}
+
